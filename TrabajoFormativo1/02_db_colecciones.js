787be15eae6165da6aec664311ef7580bd18b60c
sicosis3dfx <sicosis3dfx@outlook.com>
2026-04-20 10:36:10 -0400

    asd

---
+++ TrabajoFormativo1/02_db_colecciones.js
@@ -0,0 +1,11 @@
+// Paso 1: Utilizar la base de datos.
+"use tiendaEnLinea"
+db.createCollection("clientes")
+
+//Resultado
+console.log({ok: 1 })
+
+//Paso 3: 
+db.cientes.drop()
+// Resultado
+console.log(true)
