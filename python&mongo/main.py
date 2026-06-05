64a7cdb4646f625ce29c12033735a42815b72f0e
sicosis3dfx <sicosis3dfx@outlook.com>
2026-06-05 11:12:56 -0400

    update

---
+++ python&mongo/main.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+from pymongo import MongoClient
+
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+
+try:
+    client.admin.command("ping")
+    print(" Conexión establecida 😁 👌")
+except:
+    print(" ❌ Error de conexión 🤣 🤣")
+    exit(code=1)
+
+db = client["ti3032_u3"]
+coleccion = db["test"]
